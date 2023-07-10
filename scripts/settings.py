9a61f8f9eca5e9872d03d2e296e9b1821dc8a4ce
Tom Haelbich <haelbito@outlook.com>
2023-07-11 00:24:12 +0200

    syntax for extra params can be changed in settings now

---
+++ scripts/settings.py
@@ -0,0 +1,42 @@
+import modules.scripts as scripts
+import gradio as gr
+import os
+
+from modules import shared
+from modules import script_callbacks
+
+def on_ui_settings():
+    section = ("batchCP ", "batch checkpoint prompt")
+    shared.opts.add_option(
+        key = "promptRegex",
+        info = shared.OptionInfo(
+            "{prompt}",
+            "Prompt placeholder",
+            section=section)
+    )
+
+    shared.opts.add_option(
+        key = "batchCountRegex",
+        info = shared.OptionInfo(
+            "\{\{count:[0-9]+\}\}",
+            "Batch count Regex",
+            section=section)
+    )
+
+    shared.opts.add_option(
+        key = "clipSkipRegex",
+        info = shared.OptionInfo(
+            "\{\{clip_skip:[0-9]+\}\}",
+            "Clip skip Regex",
+            section=section)
+    )
+
+    shared.opts.add_option(
+        key = "negPromptRegex",
+        info = shared.OptionInfo(
+            "\{\{neg:(.*?)\}\}",
+            "negative Prompt Regex",
+            section=section)
+    )
+
+script_callbacks.on_ui_settings(on_ui_settings)
